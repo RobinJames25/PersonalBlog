e4fb40f9fca5342e0bba3fa4445ec3a0b13db378
RobinJames25 <jamesrobin12432@gmail.com>
2026-04-14 12:43:45 +0300

    Updated the personal blog

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user.model');
+const User = require('../models/user');
 
 exports.protect = async (req, res, next) => {
   let token;
